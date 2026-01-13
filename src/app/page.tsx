aef482e2d9b37ea1fdfe488c18c72c9cdcedf385
unknown <phuongduy@evol.vn>
2026-01-13 21:21:49 +0700

    feat: remove unused envConfig import and console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import envConfig from "../../config";
 
 export default function Home() {
-  console.log('envConfig', envConfig)
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
